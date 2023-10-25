b9edf6f2dd419d15e2ab72009182cf9fe84dda1c
Novil Fahlevy <43428328+novilfahlevy@users.noreply.github.com>
2023-10-25 20:46:40 +0800

    optimize logout code

--- lib/view_models/logout_view_model.dart
+++ lib/view_models/logout_view_model.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LogoutViewModel extends ChangeNotifier {
-  bool isLoggingOut = false;
+  bool _isLoggingOut = false;
+
+  bool get isLoggingOut => _isLoggingOut;
+
+  set isLoggingOut(bool isLoggingOut) {
+    _isLoggingOut = isLoggingOut;
+    notifyListeners();
+  }
 
   Future<bool> logout() async {
     isLoggingOut = true;
-    notifyListeners();
-
+    
     final prefs = await SharedPreferences.getInstance();
-    bool isLogoutSucceed = false;
 
     if (prefs.getString('userId') != null || prefs.getString('userId') != '') {
-      await prefs.remove('userId'); 
-      isLogoutSucceed = true;
+      await prefs.remove('userId');
+
+      isLoggingOut = false;
+      return true;
     }
 
     isLoggingOut = false;
-    notifyListeners();
-
-    return isLogoutSucceed;
+    return false;
   }
 }
